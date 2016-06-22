f9e6f5f893d224518f7c130fa320b8613ccf9c78
ChefDK User <chef@ip-10-235-170-199.ec2.internal>
2016-06-22 15:23:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_apache_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'chef_apache_cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef_apache_cookbook'
+long_description 'Installs/Configures chef_apache_cookbook'
+version '0.1.0'
