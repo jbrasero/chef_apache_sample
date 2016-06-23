d9ba474535f58167f786eae22b10002941e8861a
jbrasero <jesusbrasero78@gmail.com>
2016-06-23 14:05:22 +0000

    Update default recipe to use template

--- recipes/server.rb
+++ recipes/server.rb
@@ -10,8 +10,8 @@ package 'httpd' do
 	action :install
 end
 
-file '/var/www/html/index.html' do
-   content '<h1> Hello, world!</h1>'
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
 
 service 'httpd' do
@@ -22,7 +22,17 @@ file '/etc/motd' do
  content "Property of Jesus Brasero
    IPADDRESS: #{node['ipaddress']}
    HOSTNAME: #{node['hostname']}
-   MEMORY: #{node['total']}
+   MEMORY: #{node['memory']['total']}
    CPU: #{node['cpu']['0']['mhz']} Mhz
 "
 end
+
+#file '/var/www/html/index.html' do
+ # content "<h1>Hello, Jesus!</h1>
+#<h2>ipaddress: #{node['ipaddress']}</h2>
+#<H2>hostname: #{node['hostname']}</h2>
+#<h2>#{node['memory']['total']}</h2>
+#<h2>#{node['cpu']['0']['mhz']} Mhz</h2>
+#"
+#end
+
